c32666d841d9a2a6e0c1b6bb10c9738852be858b
Ashish Garg <ashishgarg0010@gmail.com>
2024-09-14 23:52:16 +0530

    Added product display page

--- frontend/src/Components/Breadcrumbs/Breadcrumbs.jsx
+++ frontend/src/Components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './Breadcrumbs.css'
+import arrow_icon from '../Assets/arrow_icon.svg'
 
-function Breadcrumbs() {
+function Breadcrumbs(props) {
+  const {product} = props 
   return (
-    <div>Breadcrumbs</div>
+    <div className='breadcrum'>
+      HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon } alt="" /> {product.name}
+    </div>
   )
 }
 
